43a2790bd7982d377beacde93b2bfbacf4d4f469
root <root@lab4.4ct2a5tejmyuxj23hl21uokfdd.ix.internal.cloudapp.net>
2017-11-29 09:23:50 +0000

    Update AddRemoveSSHKey.sh

--- AddRemoveSSHKey.sh
+++ AddRemoveSSHKey.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo ssh-keygen -f "/root/.ssh/known_hosts" -R 50.17.161.139
-sudo ssh-keygen -f "/root/.ssh/known_hosts" -R 34.236.181.148
-sudo ssh-keygen -f "/root/.ssh/known_hosts" -R 34.228.33.92
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 50.17.161.139
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 34.236.181.148
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 34.228.33.92
 
-sudo ssh-keyscan -H  50.17.161.139 >>  /root/.ssh/known_hosts
-sudo ssh-keyscan -H 34.236.181.148 >>  /root/.ssh/known_hosts
-sudo ssh-keyscan -H 34.228.33.92 >>  /root/.ssh/known_hosts
+sudo ssh-keyscan -H  50.17.161.139 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 34.236.181.148 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 34.228.33.92 >>  ~/.ssh/known_hosts
