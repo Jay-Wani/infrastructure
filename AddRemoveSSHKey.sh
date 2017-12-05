fa971982bda0692e2ebdded35a67a304ed3034bd
yogendra8singh <yogendra8singh@gmail.com>
2017-12-05 19:02:35 +0530

    Update AddRemoveSSHKey.sh

--- AddRemoveSSHKey.sh
+++ AddRemoveSSHKey.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 50.17.161.139
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 34.236.181.148
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 34.228.33.92
+ssh-keygen -f ~/.ssh/known_hosts -R 50.17.161.139
+ssh-keygen -f ~/.ssh/known_hosts -R 34.236.181.148
+ssh-keygen -f ~/.ssh/known_hosts -R 34.228.33.92
 
-sudo ssh-keyscan -H  50.17.161.139 >>  ~/.ssh/known_hosts
-sudo ssh-keyscan -H 34.236.181.148 >>  ~/.ssh/known_hosts
-sudo ssh-keyscan -H 34.228.33.92 >>  ~/.ssh/known_hosts
+ssh-keyscan -H  50.17.161.139 >>  ~/.ssh/known_hosts
+ssh-keyscan -H 34.236.181.148 >>  ~/.ssh/known_hosts
+ssh-keyscan -H 34.228.33.92 >>  ~/.ssh/known_hosts
